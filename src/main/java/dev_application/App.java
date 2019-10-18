bc35db19cd11bb50fa27c31e7bdc3b2a012bdcfa
Tovar, Victor <Victor.Tovar@experian.com>
2019-10-18 17:31:08 -0500

    se configura clase principal v3

--- src/main/java/dev_application/App.java
+++ src/main/java/dev_application/App.java
@@ -3,17 +3,17 @@ package dev_application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App //extends SpringBootServletInitializer
-{
-	//@Override
-	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	    return application.sources(App.class);
-	  }
-	  
-	  public static void main(String[] args) throws Exception {
-	    SpringApplication.run(App.class, args);
-	  }
+public class App extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+		return application.sources(App.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(App.class, args);
+	}
 }
