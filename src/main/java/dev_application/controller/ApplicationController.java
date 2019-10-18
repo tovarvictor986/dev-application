cd72470cf7bf77a974cd4820cd155a76f4a4e67d
Tovar, Victor <Victor.Tovar@experian.com>
2019-10-18 15:52:44 -0500

    base project

---
+++ src/main/java/dev_application/controller/ApplicationController.java
@@ -0,0 +1,22 @@
+package dev_application.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(value = "application/")
+public class ApplicationController {
+	
+	@RequestMapping(value = "/users/get")
+	@CrossOrigin(origins = "*", methods= {RequestMethod.GET})
+	public String GetUsers()  {
+		
+//		ResponseEntity<String> response = null;
+
+		return "hola";
+	}
+
+}
