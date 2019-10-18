9e5b5c40fb81dbc108dd512996186f17b99fdce2
Tovar, Victor <Victor.Tovar@experian.com>
2019-10-18 17:29:04 -0500

    se configura clase principal v2

--- src/main/java/dev_application/App.java
+++ src/main/java/dev_application/App.java
@@ -4,14 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 //import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App extends SpringBootServletInitializer
+public class App //extends SpringBootServletInitializer
 {
-	@Override
+	//@Override
 	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		  
 	    return application.sources(App.class);
 	  }
 	  
